cb3e99e5f3f3758f4db2be37ac4929ce3613c303
michelmiyazawa <daichi16387@gmail.com>
2020-11-25 05:49:26 +0000

    マイページ

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+          # sessionsコントローラーで使用する定義
+  def active_for_authentication?
+    super && (self.is_deleted == false)
+  end
 end
