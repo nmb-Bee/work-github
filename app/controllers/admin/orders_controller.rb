d9a2c352a3449ecc22b4290999410b5e723eafce
michiho1995 <micimicimici0224@gmail.com>
2020-11-27 14:57:50 +0000

    a

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,9 @@
 class Admin::OrdersController < ApplicationController
-  
+
   before_action :authenticate_admin!
 
   def index
     @orders = Order.all.page(params[:id]).per(10)
-    byebug
   end
 
   def show
