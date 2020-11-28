e5559aae1249df6de0c8f32e0ada366107a5709b
michiho1995 <micimicimici0224@gmail.com>
2020-11-28 11:13:14 +0000

    app/controllers/admin/orders_controller.rb

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -14,7 +14,7 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
-       redirect_to admin_order_path(@order)
+       redirect_to request.referer
     else
        render 'show'
     end
